3d75e53c8055bd9c7bdfe54edc4542a1792fc922
Mitul Patel <me@mitulpa.tel>
2024-12-10 09:54:03 -0500

    feat(types): added game types

---
+++ src/types/global.d.ts
@@ -0,0 +1,12 @@
+export type Player = 1 | 2;
+export type Cell = Player | null;
+export type Board = Cell[][];
+
+export interface GameState {
+  board: Board;
+  currentPlayer: Player;
+  winner: Player | null;
+  isGameOver: boolean;
+  scores: Record<Player, number>;
+  timeLeft: number;
+}
